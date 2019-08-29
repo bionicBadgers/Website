e9dd44f6a00e75d389b6af4f13a85e53f54113b8
Badger <54650852+bionicBadgers@users.noreply.github.com>
2019-08-29 12:31:19 -0600

    Website Files
    Purple Phase 2, untouched, base Website Files

---
+++ templates/myFoot.js
@@ -0,0 +1,101 @@
+Vue.component('foot', 
+	{
+  template: 
+	`
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-3 col-sm-6">
+						<h3>About Us</h3>
+						<div class="mt-25">
+							<img alt="footer-logo" src="../images/Company-Logos-Pictures/Phase 2 Logo Expanded.png">
+							<p class="mt-25">This is where we talk about our media platforms and inclued links (int the bottom) of all the platforms we use.</p>
+								<div class="mt-25 footer-social-icons">
+									<ul>
+										<li>
+											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+												<i class="fab fa-facebook-square"></i>
+											</a>
+										</li>
+										<li>
+											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+												<i class="fab fa-instagram"></i>
+											</a>
+										</li>
+										<li>
+											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+												<i class="fab fa-pinterest"></i>
+											</a>
+										</li>
+										<li>
+											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+												<i class="fab fa-skype"></i>
+											</a>
+										</li>
+										<li>
+											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+												<i class="fab fa-youtube"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-3">
+						<h3>Credited</h3>
+						<ul class="footer-list">
+							<li>
+								<a href="https://unsplash.com/@xdesro"> Some of our photos were taken by Henry Desro</a>
+							</li>
+							<li>
+								<a href="https://unsplash.com/@camorin13"> Some of our photos were taken by Cam Morin</a>
+							</li>
+							<li>
+								<a href="https://unsplash.com/@helloquence"> Some of our photos were taken by Helloquence</a>
+							</li>
+							<li>
+								<a href="https://icons8.com/material-icons">Some icons used on this page are from Icons8</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-3">
+						<h3>Useful Links</h3>
+						<ul class="footer-list">
+							<li>
+								<a href="#"> Place Useful Link Here</a>
+							</li>
+							<li>
+								<a href="#"> Place Useful Link Here</a>
+							</li>
+							<li>
+								<a href="#"> Place Useful Link Here</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-3">
+						<h3>Tags</h3>
+						<div class="footer-tags">
+							<a href="#">Business</a>
+							<a href="#">Light Gauge</a>
+							<a href="#">Steel Framing</a>
+							<a href="#">Drywall</a>
+							<a href="#">Gypsum Drywall</a>
+							<a href="#">Insulation</a>
+							<a href="#">Acoustics</a>
+							<a href="#">Wall Covering</a>
+							<a href="#">Painting</a>
+							<a href="#">Family Business</a>
+						</div>
+					</div>
+				</div>
+				<div class="footer-bar">
+					<p>Phase 2 Company © 2018. All Rights Reserved.</p>
+				</div>
+			</div>
+		</footer>
+	`
+	}),
+	
+
+
+new Vue({ el: '#templateFoot' })
