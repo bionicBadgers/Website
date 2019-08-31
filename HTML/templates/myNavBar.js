9a8709be87918ab30303859c264376213f95c9ce
bionicBadgers <54650852+bionicBadgers@users.noreply.github.com>
2019-08-31 12:52:26 -0600

    Fix Links-Luke 1
    No Nav Bar or footer yet.

--- HTML/templates/myNavBar.js
+++ HTML/templates/myNavBar.js
@@ -1,10 +1,10 @@
-Vue.component('navigation', 
+Vue.component('navigation',
 	{
-  template: 
+  template:
 	`
 		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 			<a class="navbar-brand" href="images/Company-Logos-Pictures/Phase 2 Logo.png">
-				<img src="../images/Company-Logos-Pictures/Phase 2 Logo Expanded.png" alt="Company Logo " width="150em">
+				<img src="../../images/Company-Logos-Pictures/Phase 2 Logo Expanded.png" alt="Company Logo " width="150em">
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -12,19 +12,19 @@ Vue.component('navigation',
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item">
-						<a class="nav-link" href="Home.html">Home </a>
+						<a class="nav-link" href="../Home.html">Home </a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="Contact.html">Contact Us </a>
+						<a class="nav-link" href="../Contact.html">Contact Us </a>
 					</li>
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Departments
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="Drywall.html">Drywall</a>
-							<a class="dropdown-item" href="Acoustics.html">Acoustics</a>
-							<a class="dropdown-item" href="Paint.html">Paint</a>
+							<a class="dropdown-item" href="../Drywall.html">Drywall</a>
+							<a class="dropdown-item" href="../Acoustics.html">Acoustics</a>
+							<a class="dropdown-item" href="../Paint.html">Paint</a>
 						</div>
 					</li>
 				</ul>
@@ -32,7 +32,7 @@ Vue.component('navigation',
 		</nav>
 	`
 	}),
-	
+
 
 
 new Vue({ el: '#templateNav' })
