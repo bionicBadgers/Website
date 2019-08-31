710d832d356fa5acd7d17bdb3468598e5a9b2c64
bionicBadgers <54650852+bionicBadgers@users.noreply.github.com>
2019-08-31 13:30:11 -0600

    Gosh there's a lot of Links
    Fixin fixing, always more fixin.

--- templates/myNavBar.js
+++ templates/myNavBar.js
@@ -3,8 +3,8 @@ Vue.component('navigation',
   template:
 	`
 		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-			<a class="navbar-brand" href="../../index.html">
-				<img src="../../images/Company-Logos-Pictures/Phase 2 Logo Expanded.png" alt="Company Logo " width="150em">
+			<a class="navbar-brand" href="index.html">
+				<img src="images/Company-Logos-Pictures/Phase 2 Logo Expanded.png" alt="Company Logo " width="150em">
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -12,19 +12,16 @@ Vue.component('navigation',
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item">
-						<a class="nav-link" href="../../index.html">Home </a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="../Contact.html">Contact Us </a>
+						<a class="nav-link" href="Contact.html">Contact Us </a>
 					</li>
 					<li class="nav-item dropdown">
 						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 							Departments
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="../Drywall.html">Drywall</a>
-							<a class="dropdown-item" href="../Acoustics.html">Acoustics</a>
-							<a class="dropdown-item" href="../Paint.html">Paint</a>
+							<a class="dropdown-item" href="Our Team.html">Our Team</a>
+							<a class="dropdown-item" href="Acoustics.html">Acoustics</a>
+							<a class="dropdown-item" href="Paint.html">Paint</a>
 						</div>
 					</li>
 				</ul>
