c29d48a50ba7d6ae768d5e36dd568c3e1dc1ef4f
bionicBadgers <54650852+bionicBadgers@users.noreply.github.com>
2019-08-31 14:08:19 -0600

    Luke Again
    Added FIRST Logo, Contact Us Picture,

--- templates/myFoot.js
+++ templates/myFoot.js
@@ -8,32 +8,32 @@ Vue.component('foot',
 					<div class="col-xs-12 col-md-3 col-sm-6">
 						<h3>About Us</h3>
 						<div class="mt-25">
-							<img alt="footer-logo" src="../../images/Company-Logos-Pictures/Phase 2 Logo Expanded.png">
+							<img alt="footer-logo" src="images/Company-Logos-Pictures/Phase 2 Logo Expanded.png">
 							<p class="mt-25">This is where we talk about our media platforms and inclued links (int the bottom) of all the platforms we use.</p>
 								<div class="mt-25 footer-social-icons">
 									<ul>
 										<li>
-											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+											<a href="https://www.facebook.com/BionicBadgersFTC/?hc_ref=ARQPVz-ph4_m4UAAfMehdb3Fynr3cwFKqPa-DgP9kqVj4lAacC7y0x93d2YBe__-ZwY&fref=nf&__xts__[0]=68.ARB4wQwJbhMr569bbbp0i9SI1aOznSWNNPM2w3DXd485uPx8Lj-3MKSRF0KPLHDcurF4vYasRQjN1QnHC3qwvDLYedMc75ShjIsqMHNUZnPOfkIz4IwuAlvqcKHTPQJtY05jPT3F5Rf7oHrDFAmpK-y_fckwzBuvYrICtKyhNOWiwGkhezhiwGrZKO9S5GlVHz-qNMyXq4giKB4t1pxAKePLliR2bN2piHELJ48MqrqrOh133C7iJ69d2u104UULYf1pQ7khZ0y9ZEeUbFxESMlNs5s8LycZ_c-_ZsBkNhSy8T98-D_7m0WGbAkhA4Ql3KsR3oCooZjM34OtA2uR_-M&__tn__=kC-R.html">
 												<i class="fab fa-facebook-square"></i>
 											</a>
 										</li>
 										<li>
-											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+											<a href="#">
 												<i class="fab fa-instagram"></i>
 											</a>
 										</li>
 										<li>
-											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+											<a href="#">
 												<i class="fab fa-pinterest"></i>
 											</a>
 										</li>
 										<li>
-											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+											<a href="#">
 												<i class="fab fa-skype"></i>
 											</a>
 										</li>
 										<li>
-											<a href="http://specthemes.com/redbiz/redBiz-template-2.0/home-5.html#">
+											<a href="#">
 												<i class="fab fa-youtube"></i>
 											</a>
 										</li>
@@ -54,7 +54,7 @@ Vue.component('foot',
 								<a href="https://unsplash.com/@helloquence"> Some of our photos were taken by Helloquence</a>
 							</li>
 							<li>
-								<a href="https://icons8.com/material-icons">Some icons used on this page are from Icons8</a>
+								<a href="https://unsplash.com/@jesuskiteque">Some of our photos were taken by Jesus Kiteque</a>
 							</li>
 						</ul>
 					</div>
@@ -62,7 +62,7 @@ Vue.component('foot',
 						<h3>Useful Links</h3>
 						<ul class="footer-list">
 							<li>
-								<a href="#"> Place Useful Link Here</a>
+								<a href="https://www.firstinspires.org/robotics/ftc">First Tech Challenge</a>
 							</li>
 							<li>
 								<a href="#"> Place Useful Link Here</a>
@@ -72,7 +72,10 @@ Vue.component('foot',
 							</li>
 						</ul>
 					</div>
-					
+					<div class="col-xs-12 col-sm-6 col-md-3">
+					<img alt="First Tech Challenge Logo"  src="images/FIRST Logo.png">
+					</div>
+
 				</div>
 				<div class="footer-bar">
 					<p>Bionic Badgers of Wyoming © 2019. All Rights Reserved.</p>
