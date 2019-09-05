0e0184e9607d2992daed1ea5b57166c515e72dd8
killerseig <cjlarsen1165@gmail.com>
2019-09-05 13:18:53 -0600

    HTML Content Fixes

--- templates/myNavBar.js
+++ templates/myNavBar.js
@@ -2,9 +2,9 @@ Vue.component('navigation',
 	{
   template:
 	`
-		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="padding: 0em; padding-left: 1em; padding-right: 1em; margin: 0em;">
 			<a class="navbar-brand" href="index.html">
-				<img src="..." alt="Logo ">
+				<img src="images/team_logo_transparent_background.png" alt="Logo " style="height:50px; padding: 0em; margin: 0em;">
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
