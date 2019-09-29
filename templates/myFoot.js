7566727b61b6a7ee2f5bc6c173d83990aa7dd0bc
bionicBadgers <54650852+bionicBadgers@users.noreply.github.com>
2019-09-29 14:24:24 -0600

    Little Things

--- templates/myFoot.js
+++ templates/myFoot.js
@@ -5,11 +5,11 @@ Vue.component('foot',
 		<footer>
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12 col-md-3 col-sm-6">
+					<div class="col-sm-12 col-md-3 col-sm-6">
 						<h3>About Us</h3>
 						<div class="mt-25">
 							<img alt="footer-logo" src="images/team_logo_transparent_background.png">
-							<p class="mt-25">This is where we talk about our media platforms and inclued links (int the bottom) of all the platforms we use.</p>
+							<p class="mt-25"></p>
 								<div class="mt-25 footer-social-icons">
 									<ul>
 										<li>
@@ -61,21 +61,21 @@ Vue.component('foot',
 							</li>
 						</ul>
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-3">
+					<div class="col-s-12 col-sm-6 col-lg-3">
 						<h3>Useful Links</h3>
 						<ul class="footer-list">
 							<li>
 								<a href="https://www.firstinspires.org/robotics/ftc">First Tech Challenge</a>
 							</li>
 							<li>
-								<a href="#"> Place Useful Link Here</a>
+								<a href="https://ftcforum.usfirst.org/">FTC Forum</a>
 							</li>
 							<li>
-								<a href="#"> Place Useful Link Here</a>
+								<a href="https://www.facebook.com/BionicBadgersFTC/?hc_ref=ARQPVz-ph4_m4UAAfMehdb3Fynr3cwFKqPa-DgP9kqVj4lAacC7y0x93d2YBe__-ZwY&fref=nf&__xts__[0]=68.ARB4wQwJbhMr569bbbp0i9SI1aOznSWNNPM2w3DXd485uPx8Lj-3MKSRF0KPLHDcurF4vYasRQjN1QnHC3qwvDLYedMc75ShjIsqMHNUZnPOfkIz4IwuAlvqcKHTPQJtY05jPT3F5Rf7oHrDFAmpK-y_fckwzBuvYrICtKyhNOWiwGkhezhiwGrZKO9S5GlVHz-qNMyXq4giKB4t1pxAKePLliR2bN2piHELJ48MqrqrOh133C7iJ69d2u104UULYf1pQ7khZ0y9ZEeUbFxESMlNs5s8LycZ_c-_ZsBkNhSy8T98-D_7m0WGbAkhA4Ql3KsR3oCooZjM34OtA2uR_-M&__tn__=kC-R.html">Bionic Badgers Facebook</a>
 							</li>
 						</ul>
 					</div>
-					<div class="col-xs-12 col-sm-6 col-md-3">
+					<div class="col-md-12 col-sm-6 col-md-3">
 					<img alt="First Tech Challenge Logo"  src="images/FIRST Logo.png">
 					</div>
 
