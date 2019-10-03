16c260ca936a6533366097f5ae701e7b8b159ee4
bionicBadgers <54650852+bionicBadgers@users.noreply.github.com>
2019-10-02 18:43:17 -0600

    Footer Content

--- templates/myFoot.js
+++ templates/myFoot.js
@@ -48,32 +48,23 @@ Vue.component('foot',
 								<a href="https://www.firstinspires.org/robotics/ftc">First Tech Challenge</a>
 							</li>
 							<li>
-								<a href="#"> Place Useful Link Here</a>
+								<a href="https://ftcforum.usfirst.org">FTC Forum</a>
 							</li>
 							<li>
-								<a href="#"> Place Useful Link Here</a>
+								<a href="https://www.facebook.com/BionicBadgersFTC/?hc_ref=ARQPVz-ph4_m4UAAfMehdb3Fynr3cwFKqPa-DgP9kqVj4lAacC7y0x93d2YBe__-ZwY&fref=nf&__xts__[0]=68.ARB4wQwJbhMr569bbbp0i9SI1aOznSWNNPM2w3DXd485uPx8Lj-3MKSRF0KPLHDcurF4vYasRQjN1QnHC3qwvDLYedMc75ShjIsqMHNUZnPOfkIz4IwuAlvqcKHTPQJtY05jPT3F5Rf7oHrDFAmpK-y_fckwzBuvYrICtKyhNOWiwGkhezhiwGrZKO9S5GlVHz-qNMyXq4giKB4t1pxAKePLliR2bN2piHELJ48MqrqrOh133C7iJ69d2u104UULYf1pQ7khZ0y9ZEeUbFxESMlNs5s8LycZ_c-_ZsBkNhSy8T98-D_7m0WGbAkhA4Ql3KsR3oCooZjM34OtA2uR_-M&__tn__=kC-R.html">Facebook Page</a>
 							</li>
 						</ul>
 					</div>
 
-					
+
 					<div class="col-sm-12 col-sm-6 col-lg-3">
 						<h3>Credited</h3>
 						<ul class="footer-list">
 							<li>
-								<a href="https://unsplash.com/@xdesro"> Some of our photos were taken by Henry Desro</a>
-							</li>
-							<li>
-								<a href="https://unsplash.com/@camorin13"> Some of our photos were taken by Cam Morin</a>
-							</li>
-							<li>
-								<a href="https://unsplash.com/@helloquence"> Some of our photos were taken by Helloquence</a>
-							</li>
-							<li>
 								<a href="https://unsplash.com/@jesuskiteque">Some of our photos were taken by Jesus Kiteque</a>
 							</li>
 							<li>
-								<a href="https://unsplash.com/@jesuskiteque">Some of our photos were taken by Rock'n Roll Monkey</a>
+								<a href="https://unsplash.com/@rocknrollmonkey">Some of our photos were taken by Rock'n Roll Monkey</a>
 							</li>
 						</ul>
 					</div>
